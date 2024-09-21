3cddac872ec226310ea56b79cd4f7208a56fa8c7
Mohamed Abdelhady <mohamed.abdelhady.ahmed1@gmail.com>
2024-09-21 15:27:15 +0300

    final updatev5

--- admin/add_category.php
+++ admin/add_category.php
@@ -12,7 +12,7 @@ include_once "layouts/sidebar.php";
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Add Book</h1>
+          <h1>Add Category</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
